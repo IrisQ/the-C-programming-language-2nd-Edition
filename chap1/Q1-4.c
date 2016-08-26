3f17e4139a17bfe1412a6e7887932e982d14dc77
IrisQ <eloise_zyf@hotmail.com>
2016-08-26 10:36:05 -0400

    Add files via upload

---
+++ chap1/Q1-4.c
@@ -0,0 +1,24 @@
+/********************************************
+Print table of celsius from 0-150 (step is 20)
+and corresponding celsius temperature.
+********************************************/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0; 		/* lower bound */
+	upper = 150;	/* upper bound */
+	step = 10; 
+
+	celsius = lower;
+	printf("%s\n", "Celsius vs Fahr:");
+	while (celsius <= upper) {
+		fahr = (9.0/5.0) * celsius + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+	return 0;
+}
