74fc99190b79cf2c54b64570e2e90d0ee699cd1b
IrisQ <eloise_zyf@hotmail.com>
2016-08-26 10:27:34 -0400

    Add files via upload

---
+++ chap1/Q1-2.c
@@ -0,0 +1,8 @@
+/****************************************
+what would happen if string contains '\c'
+*****************************************/
+#include <stdio.h>
+int main(){
+	printf("hello,world!\c\n");
+	return 0;
+}
